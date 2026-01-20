a9f0f65d9be5a156affd7964cdfaf0d0893b9a36
laxmi5336 <laxmibandeppagoudra@gmail.com>
2026-01-20 19:48:33 +0530

    Added AutoConfigureTestDatabase annotation

--- src/test/java/com/ls/grocery_app/GroceryAppApplicationTests.java
+++ src/test/java/com/ls/grocery_app/GroceryAppApplicationTests.java
@@ -1,9 +1,11 @@
 package com.ls.grocery_app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class GroceryAppApplicationTests {
 
 	@Test
